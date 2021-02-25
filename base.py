de21d6bca43380c5409737c96a14fe1a6a3be7ad
Jeremy Kellem <60828719+kgsensei@users.noreply.github.com>
2021-02-25 07:58:38 -0500

    Update base.py

--- base.py
+++ base.py
@@ -1,9 +1,12 @@
-import pyautogui as pya
-from pynput import mouse
+try:
+    import pyautogui, keyboard
+except Exception:
+    import os
+    os.system("pip install pyautogui")
+    os.system("pip install keyboard")
+    import pyautogui, keyboard
 
-def on_click(x, y, button, pressed):
-    if pressed:
-        pya.click(button='right')
-
-with mouse.Listener(on_click=on_click) as listener:
-    listener.join()
+x=input("AutoClicker Key Bind: ")
+while True:
+    if keyboard.is_pressed(x):
+        pyautogui.click(button='right')
